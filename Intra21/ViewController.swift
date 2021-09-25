647d94050ca43d8833e18134c5cb539208500e10
Mihail Nazarov <msnazarow@gmail.com>
2021-09-25 21:10:50 +0300

    Initial Commit

---
+++ Intra21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intra21
+//
+//  Created by out-nazarov2-ms on 25.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
