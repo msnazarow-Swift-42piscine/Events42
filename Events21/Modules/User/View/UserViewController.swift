9a004e3bcfa3f7346371582e9346494d6cf50a9b
Vladislav Koshelev <VKoshelev@detmir.ru>
2021-09-26 17:25:03 +0300

    add user screen

--- Events21/Modules/User/View/UserViewController.swift
+++ Events21/Modules/User/View/UserViewController.swift
@@ -12,6 +12,81 @@ import UIKit
 
 
 class UserViewController: UIViewController {
+    
+    // MARK: - UI
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        // remove after test
+        imageView.image = UIImage(systemName: "person")
+        imageView.backgroundColor = .cyan
+        return imageView
+    }()
+    
+    let loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .boldSystemFont(ofSize: 15)
+        label.textAlignment = .left
+        // remove after test
+        label.text = "ebulwer"
+        return label
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14)
+        label.textAlignment = .left
+        // remove after test
+        label.text = "Effrafax"
+        return label
+    }()
+    
+    let surnameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14)
+        label.textAlignment = .left
+        // remove after test
+        label.text = "Bulwer"
+        return label
+    }()
+    
+    let levelLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14)
+        // remove after test
+        label.text = "Current level: 7.88"
+        return label
+    }()
+    
+    lazy var verticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [loginLabel, nameLabel, surnameLabel, levelLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 8
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    lazy var horizontalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [profileImageView, verticalStackView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
 
     // MARK: - Properties
     var presenter: ViewToPresenterUserProtocol!
@@ -25,15 +100,30 @@ class UserViewController: UIViewController {
 
     private func setupUI() {
         addSubviews()
+        tableView.dataSource = presenter?.dataSource
+//        tableView.delegate = self
         setupConstraints()
     }
 
     private func addSubviews() {
-
+        view.addSubview(horizontalStackView)
+        view.addSubview(tableView)
     }
 
     private func setupConstraints() {
-
+        view.layoutMargins = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        view.backgroundColor = .white
+        
+        NSLayoutConstraint.activate([
+            horizontalStackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            horizontalStackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            horizontalStackView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: horizontalStackView.bottomAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 }
 
