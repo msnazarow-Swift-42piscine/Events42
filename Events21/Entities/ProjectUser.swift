fac487be2df6b2020705ff3db44111ee1b10685e
msnazarow <msnazarow@gmail.com>
2022-02-03 11:41:27 +0300

    fix: add createGroup status

--- Events21/Entities/ProjectUser.swift
+++ Events21/Entities/ProjectUser.swift
@@ -23,6 +23,7 @@ struct ProjectUser: Codable {
 		case finished
 		case inProgress = "in_progress"
 		case searchingAGroup = "searching_a_group"
+		case creatingGroup = "creating_group"
 		case parent
 	}
 
